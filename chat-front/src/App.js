a1b3d5c144f49e886932a26e088ab3d3fe7942fd
Maksim <lyzlov023@gmail.com>
2024-03-24 16:49:00 +1000

    initial frontend commit

---
+++ chat-front/src/App.js
@@ -0,0 +1,28 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Connection } from "./features/connection/Connection";
+
+function App() {
+  return (
+    <div className="App">
+      <Connection />
+
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
