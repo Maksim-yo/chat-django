433d72f7c29c28cac7df0da07c6a1f77dac9920c
Maksim <lyzlov023@gmail.com>
2024-04-07 20:12:36 +1000

    Add logout endpoint

--- chat-front/src/app/services/api/apiService.js
+++ chat-front/src/app/services/api/apiService.js
@@ -1,11 +1,18 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { useHistory } from "react-router-dom";
-
+import Cookies from "js-cookie";
 const baseUrl = "http://127.0.0.1:8000/";
 
 export const authApi = createApi({
   reducerPath: "auth_api",
-  baseQuery: fetchBaseQuery({ baseUrl }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: baseUrl,
+    credentials: "include",
+    prepareHeaders: (headers, { getState }) => {
+      headers.set("X-CSRFToken", Cookies.get("csrftoken"));
+      return headers;
+    },
+  }),
   endpoints: (build) => ({
     postLogin: build.mutation({
       query: (body) => ({
@@ -22,21 +29,42 @@ export const authApi = createApi({
         body,
       }),
     }),
+
+    postLogout: build.mutation({
+      queryFn: async (_, { getState }) => {
+        const token = getState().auth.userToken;
+        const response = await fetch(`${baseUrl}accounts/logout/`, {
+          method: "POST",
+          headers: token ? { Authorization: `Token ${token}` } : {},
+        });
+
+        if (!response.ok) {
+          return {
+            error: {
+              status: response.status,
+              data: {
+                message: response?.statusText,
+              },
+            },
+          };
+        }
+
+        return await { data: response.json() };
+      },
+    }),
   }),
 });
 
 const baseQuery = fetchBaseQuery({
   baseUrl,
-  // mode: "no-cors",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.userToken;
     console.log(token);
     if (token) {
       headers.set("authorization", `Token ${token}`);
       headers.set("Content-Type", "application/json");
-
-      return headers;
     }
+    return headers;
   },
 });
 
@@ -59,7 +87,9 @@ export const appApi = createApi({
   }),
 });
 
-// export react hook
-// export const { useGetUserQuery } = authApi;
-export const { usePostLoginMutation, usePostSignUpMutation } = authApi;
+export const {
+  usePostLoginMutation,
+  usePostSignUpMutation,
+  usePostLogoutMutation,
+} = authApi;
 export const { useGetUserDetailsQuery } = appApi;
--- chat-front/src/components/Logout/Logout.js
+++ chat-front/src/components/Logout/Logout.js
@@ -1,14 +1,22 @@
 import { logout } from "../../features/auth/authSlice";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-
-const Logouta = () => {
+import { usePostLogoutMutation } from "../../app/services/api/apiService";
+const Logout = () => {
   const dispatch = useDispatch();
+  const [postLogout, { isLoading, error, isError, isSuccess }] =
+    usePostLogoutMutation();
+
   useEffect(() => {
-    dispatch(logout());
+    postLogout();
   }, []);
+
+  useEffect(() => {
+    if (isSuccess) dispatch(logout());
+    if (error) console.log(error);
+  }, [isSuccess, error]);
 
   return null;
 };
 
-export default Logouta;
+export default Logout;
